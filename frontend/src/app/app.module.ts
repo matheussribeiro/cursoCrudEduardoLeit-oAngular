7beafbca309d75f96a6ebe80e14493712e98a55e
matheussribeiro <mtsales1337@gmail.com>
2022-01-26 11:44:03 -0300

    create service and add snackbar in product-create

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -12,6 +12,7 @@ import {MatSidenavModule}from '@angular/material/sidenav';
 import {MatCardModule}from '@angular/material/card';
 import {MatListModule}from '@angular/material/list';
 import {MatButtonModule}from '@angular/material/button';
+import {MatSnackBarModule}from '@angular/material/snack-bar';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { RedDirective } from './directives/red.directive';
@@ -37,7 +38,8 @@ import { ProductCreateComponent } from './components/product/product-create/prod
     MatSidenavModule,
     MatListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
